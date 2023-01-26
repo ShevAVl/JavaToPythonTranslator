c4341cc82b46c3ea5f0b1e66c5b711ab9bae778c
ShevAVl <sh.alvl@outlook.com>
2023-01-26 14:02:54 +1000

    Add files via upload
    Uploading a valid debug build

---
+++ JavaInPythonTranslator/JavaInPythonTranslator/bin/Debug/net6.0/Output/TestCorrect.py
@@ -0,0 +1,27 @@
+
+
+def main():
+	a = 25 
+	print(a + a * (a + 1)exp , a exp exp )
+	if(a >= (not True ) and True exp ):
+		a += 25 exp 
+		exp exp 
+	else:
+		a = 20 exp 
+		exp exp exp 
+	a=0
+	for a in range(0, 5, (a + 1)):
+		print(a * a exp exp )
+		exp exp 
+	while(a > 0 exp ):
+		print(a * a exp exp )
+		break 
+		exp exp exp 
+	print(a * a exp exp )
+	exp while(a > 0 ):
+		print(a * a exp exp )
+		exp 
+	exp exp exp exp exp exp exp exp 
+exp exp 
+if __name__ == '__main__':
+	main()
